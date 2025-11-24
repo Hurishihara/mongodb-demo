94990387ac243dbb803d8231c2cc4b33f324a8f4
Hurishihara <azgelo21@gmail.com>
2025-11-24 18:38:50 +0800

    change .env

--- test.uri.js
+++ test.uri.js
@@ -1,5 +1,5 @@
 import 'dotenv/config';
 
-const username = process.env.username;
-const password = process.env.password;
+const username = process.env.mongoDBUsername;
+const password = process.env.mongoDBPassword;
 export const uri = `mongodb+srv://${username}:${password}@cluster0.g9t1hx1.mongodb.net/?appName=Cluster0`
