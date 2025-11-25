3c5f81edc741f0ab81be1ee0d747160137b59ee2
Hurishihara <azgelo21@gmail.com>
2025-11-25 14:26:50 +0800

    Add MongoDB document operations and update package.json scripts

--- index.js
+++ index.js
@@ -15,10 +15,68 @@ const listDatabases = async (client) => {
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 }
 
+const insertOneDocument = async (client) => {
+    const result = await client.db('school')
+        .collection('grades')
+        .insertOne({
+            student_id: 654321,
+            products: [
+                {
+                    type: 'exam',
+                    score: 90
+                },
+                {
+                    type: 'homework',
+                    score: 59
+                },
+                {
+                    type: 'quiz',
+                    score: 75
+                },
+                {
+                    type: 'homework',
+                    score: 88
+                }
+            ],
+            class_id: 550,
+        })
+    console.log(`New document inserted with the following id: ${result.insertedId}`);
+}
+
+const upsertOneDocument = async (client) => {
+    const result = await client.db('school').collection('grades')
+        .updateOne(
+            { student_id: 112333 },
+            { $set: { student_id: 112333, class_id: 777, } },
+            { upsert: true }
+        )
+    console.log(`${result.matchedCount} document(s) matched the filter, ${result.upsertedCount} document(s) upserted, ${result.modifiedCount} document(s) modified.`);
+}
+
+const countDocumnents = async (client) => {
+    const count = await client.db('school').collection('grades')
+        .countDocuments()
+    
+    console.log(`Total documents in the collection: ${count}`);
+}
+
+const updatePushDocument = async (client) => {
+    const result = await client.db('school').collection('grades')
+        .updateOne(
+            { student_id: 654321 },
+            { $push: { products: { type: 'Assessment Test', score: 98 } } }
+        )
+    console.log(`${result.matchedCount} document(s) matched the filter, ${result.modifiedCount} document(s) modified.`)
+}
+
 const run = async () => {
     try {
         await client.connect();
-        await listDatabases(client);
+        //await listDatabases(client);
+        //await insertOneDocument(client);
+        //await upsertOneDocument(client);
+        //await updatePushDocument(client);
+        await countDocumnents(client);
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
